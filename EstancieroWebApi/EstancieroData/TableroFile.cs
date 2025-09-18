7812788e3920a9d59b78b9e430e9c61adb8ce54f
Modificacion <gasalusso@gmail.com>
2025-09-18 18:48:47 -0300

    5to commit: anexo tablero

---
+++ EstancieroWebApi/EstancieroData/TableroFile.cs
@@ -0,0 +1,26 @@
+﻿using EstancieroEntities;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Estanciero.Data
+{
+    public class TableroFile
+    {
+        public static string archivo = Path.GetFullPath("datos/tablero.json");
+
+        public static List<CasilleroEntity> LeerTablero()
+        {
+            if (File.Exists(archivo))
+            {
+                string json = File.ReadAllText(archivo);
+                return JsonConvert.DeserializeObject<List<CasilleroEntity>>(json);
+            }
+            else
+            {
+                return new List<CasilleroEntity>();
+            }
+        }
+    }
+}
+
